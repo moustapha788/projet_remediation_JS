aa1a08de381ea43387052a9f42a617280c249dcf
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-03-17 17:43:00 +0000

    US1 Sprint1 :TP1_Notification_1. Générer des notifications en fonction de la couleur du bouton....2. Les notifications s’affichent en pile et on une durée de 1 seconde

---
+++ TP1_Notification/script.js
@@ -0,0 +1,37 @@
+/* 
+ * Récupérer les éléments
+ */
+theBtns = document.querySelectorAll('.container-btn button');
+containerNotification = document.querySelector('.container-notification');
+/* 
+ * Events 
+ */
+
+theBtns.forEach(btn => { btn.addEventListener("click", e => showNotification(e)) });
+/* 
+ * Functions 
+ */
+// ! showNotification
+function showNotification(e) {
+    const small = createTheSmallTag();
+    getAndSetFuturies(e, small);
+    containerNotification.appendChild(small);
+    setTimeout(() => containerNotification.removeChild(small), 3000);
+}
+// ! getAndSetFuturies
+function getAndSetFuturies(e, small) {
+    small.innerHTML = e.target.innerHTML.replace("Notification", "");
+    small.classList.add(e.target.className, "show-notification", "animate__bounce");
+}
+// ! createTheSmallTag
+function createTheSmallTag() { return document.createElement('small') }
+/* "getStyle" est une fonction JavaScript qui utilise la méthode "getComputedStyle" pour récupérer la liste des styles d'un élément HML et permet de récupérer la valeur d'une propriété CSS calculée en fonction du rendu final.
+
+Très simpliste, elle prend en arguments la propriété CSS que l'on souhaite lire et l'élément duquel la récupérer. Voici un exemple pour savoir si un élément de page est affiché :
+
+getStyle('display', document.body);
+
+renvoie par exemple : "block"
+*/
+// ! getStyle
+function getStyle(a, b) { return window.getComputedStyle(b, null)[a]; }
