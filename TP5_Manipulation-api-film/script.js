e4db50a220cb5a352df102b94e361dfd08cd994a
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-03-22 18:36:13 +0000

    US5_sprint3:barre de recheche

--- TP5_Manipulation-api-film/script.js
+++ TP5_Manipulation-api-film/script.js
@@ -4,36 +4,44 @@ const input = document.querySelector(".header form input[type='search']");
 
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 /* 
 ***********
 EVENTS
 ***********
 */
 getMovies(APIURL);
+form.addEventListener("input", e => { searchMovie(e.target.value) });
 
-form.addEventListener("submit", () => {
-    searchMovie();
-});
+
 
 /* 
 ***********
 FUNCTIONS
 ***********
 */
+
 // asynchrous function who gets the movies
 async function getMovies(url) {
-    if (fetch(APIURL)) {
-        const response = await fetch(APIURL);
+    try {
+        const response = await fetch(url);
         var data = await response.json();
         createACardMovie(data["results"]);
+    } catch (error) {
+        console.log(error.message);
     }
 }
 
 // create a movie
 function createACardMovie(tableData) {
     tableData.forEach(movie => {
-        const averageClass = (movie["vote_average"] <= 7) ? "orange" : "top";
+        if (movie["vote_average"] < 5) {
+            averageClass = "red";
+        } else if (movie["vote_average"] < 7) {
+            averageClass = "orange";
+        } else {
+            averageClass = "top";
+        }
         const card = `
             <div class="card">
                 <div class="movie-image">
@@ -44,7 +52,7 @@ function createACardMovie(tableData) {
                     <small class="${averageClass}">${movie["vote_average"]}</small>
                 </div>
                 <div class="overview">
-                    <h1>Overview</h1>
+                <h1>Overview</h1>
                     <p>${movie["overview"]}</p>
                 </div>
             </div>
@@ -56,12 +64,14 @@ function createACardMovie(tableData) {
 function viderMain() {
     main.innerHTML = "";
 }
+
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 // searchMovie
-function searchMovie(data) {
-    const request = input.value;
-    // if (data["title"] === request) {
-    getMovies(SEARCHAPI + request);
-    // } else {
-    // getMovies(APIURL);
-    // }
+function searchMovie(request) {
+    if (request == "") {
+        getMovies(APIURL);
+    } else {
+        viderMain();
+        getMovies(SEARCHAPI + request);
+    }
 }
